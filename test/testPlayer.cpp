e52093ce414b3e024b636edefb9b8f13ed131654
Miguel <mzorrilla@fi.uba.ar>
2025-06-29 21:24:44 -0300

    Arreglando test

--- test/testPlayer.cpp
+++ test/testPlayer.cpp
@@ -10,5 +10,12 @@ TEST_CASE("Jugador puede obtener un bloque") {
       std::make_unique<Bloque>("Pasticho", "Melon");
   std::unique_ptr<Player> player = std::make_unique<Player>("Pedro", 4);
   player->agarrarBloque(std::move(bloque1));
-  CHECK(player->cantidadObjetosEnInventario() == 0);
+  CHECK(player->cantidadObjetosEnInventario() == 1);
+}
+
+TEST_CASE("Jugador puede obtener más de un bloque") {
+  std::unique_ptr<Player> player = std::make_unique<Player>("Pedro", 4);
+  player->agarrarBloque(std::make_unique<Bloque>("Pasticho", "Melon"));
+  player->agarrarBloque(std::make_unique<Bloque>("Pasticho", "Melon"));
+  CHECK(player->cantidadObjetosEnInventario() == 2);
 }
